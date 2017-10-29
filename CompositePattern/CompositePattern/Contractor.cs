3199de6a8cd055ab9f201b8bd71da8ad9c1d9239
Muhammad Asif <m.asif@live.dk>
2017-10-29 21:29:09 +0100

    composite pattern implementation

---
+++ CompositePattern/CompositePattern/Contractor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    public class Contractor : IEmployed
+    {
+        public int EmpID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ CompositePattern/CompositePattern/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Rahul = new Employee { EmpID = 1, Name = "Rahul-President" };
+
+            Employee Amit = new Employee { EmpID = 2, Name = "Amit-VicePresident" };
+            Employee Mohan = new Employee { EmpID = 3, Name = "Mohan-ExecutivePresident" };
+
+            Rahul.AddSubordinate(Amit);
+            Rahul.AddSubordinate(Mohan);
+
+            Employee Rita = new Employee { EmpID = 4, Name = "Rita-Director" };
+            Employee Hari = new Employee { EmpID = 5, Name = "Hari-Director" };
+
+            Amit.AddSubordinate(Rita);
+            Amit.AddSubordinate(Hari);
+
+            Employee Kamal = new Employee { EmpID = 6, Name = "Kamal-RegularEmp" };
+            Employee Raj = new Employee { EmpID = 7, Name = "Raj-RegularEmp" };
+
+            Contractor Sam = new Contractor { EmpID = 8, Name = "Sam-RegularEmp" };
+            Contractor tim = new Contractor { EmpID = 9, Name = "Tim-RegularEmp" };
+
+            Mohan.AddSubordinate(Kamal);
+            Mohan.AddSubordinate(Raj);
+            Mohan.AddSubordinate(Sam);
+            Mohan.AddSubordinate(tim);
+
+            Console.WriteLine("EmpID={0}, Name={1}", Rahul.EmpID, Rahul.Name);
+
+            foreach (Employee manager in Rahul)
+            {
+                Console.WriteLine("\n EmpID={0}, Name={1}", manager.EmpID, manager.Name);
+
+                foreach (var employee in manager)
+                {
+                    Console.WriteLine(" \t EmpID={0}, Name={1}", employee.EmpID, employee.Name);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+    }
+
